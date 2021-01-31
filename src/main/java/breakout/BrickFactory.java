bb03d4a9d7d4147aae5d134a9c9852a991cc09ee
joshu <joshuapetitma@yahoo.com>
2021-01-31 08:15:09 -0500

    Actually uses constances created 🤦

--- src/main/java/breakout/BrickFactory.java
+++ src/main/java/breakout/BrickFactory.java
@@ -1,34 +1,34 @@
 package breakout;
 
 public class BrickFactory {
-	public static int BORING_BRICK = 0;
-	public static int PAUSE_BRICK = 1;
-	public static int FIRE_BRICK = 2;
-	public static int GENERATIVE_BRICK = 3;
-	public static int HEALTH_BRICK = 4;
-	public static int SANIC_BRICK = 5;
-	public static int INVINCIBILITY_BRICK = 6;
-	public static int STRONG_BRICK = 7;
-	public static int MONEY_BRICK = 8;
-	public static int TOTAL_TYPES = 9;
+	public static final int BORING_BRICK = 0;
+	public static final int PAUSE_BRICK = 1;
+	public static final int FIRE_BRICK = 2;
+	public static final int GENERATIVE_BRICK = 3;
+	public static final int HEALTH_BRICK = 4;
+	public static final int SANIC_BRICK = 5;
+	public static final int INVINCIBILITY_BRICK = 6;
+	public static final int STRONG_BRICK = 7;
+	public static final int MONEY_BRICK = 8;
+	public static final int TOTAL_TYPES = 9;
 
 	public static Brick create(ActionListener listener, int brickType) {
 		switch(brickType) {
-			case 1:
+			case PAUSE_BRICK:
 				return new PauseBrick(listener);
-			case 2:
+			case FIRE_BRICK:
 				return new FireBrick(listener);
-			case 3:
+			case GENERATIVE_BRICK:
 				return new GenerativeBrick(listener);
-			case 4:
+			case HEALTH_BRICK:
 				return new HealthBrick(listener);
-			case 5:
+			case SANIC_BRICK:
 				return new SanicBrick(listener);
-			case 6:
+			case INVINCIBILITY_BRICK:
 				return new InvincibilityBrick(listener);
-			case 7:
+			case STRONG_BRICK:
 				return new StrongBrick(listener);
-			case 8:
+			case MONEY_BRICK:
 				return new MoneyBrick(listener);
 			default:
 				return new Brick(listener);
