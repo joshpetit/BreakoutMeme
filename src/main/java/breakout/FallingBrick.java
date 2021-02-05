c2c2b91c8be3331c43b0db5975978a3529b3ea25
joshu <joshuapetitma@yahoo.com>
2021-02-05 14:24:45 -0500

    Adds falling brick class

---
+++ src/main/java/breakout/FallingBrick.java
@@ -0,0 +1,38 @@
+package breakout;
+
+/** A brick that can fall. */
+public abstract class FallingBrick extends Brick {
+  protected boolean fallen = false;
+  protected String fallingImage;
+
+  protected FallingBrick(ActionListener listener, String image, String fallingImage) {
+    super(listener);
+    setImage(image);
+	this.fallingImage = fallingImage;
+  }
+
+  @Override
+  protected void hitBall() {
+	  hitHotBall();
+  }
+
+  @Override
+  protected void hitHotBall() {
+    if (!fallen) {
+      fall();
+      setImage(fallingImage);
+      listener.addPoints(2);
+    }
+  }
+
+  @Override
+  protected void hitHotWall() {
+	  destroy();
+  }
+
+  protected void fall() {
+    setDirectionY(1);
+    setSpeed(150);
+    fallen = true;
+  }
+}
